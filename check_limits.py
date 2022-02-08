9c5ebf05541225dde6fa5beba9db98b3db664be2
Shivam (MS/ESD-ET2-XC) <tgh1kor@bosch.com>
2022-02-08 19:26:18 +0530

    Added 1 level of abstraction and reduced cyclomatic complexity.

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,60 @@
+from abc import ABC, abstractmethod
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
 
-  return True
+class RangeService(ABC):
+    @abstractmethod
+    def is_value_in_range(self, value):
+        pass
+
+
+class Temperature(RangeService):
+    lower_limit = 0
+    higher_limit = 45
+
+    @classmethod
+    def is_value_in_range(cls, value):
+        if value in range(cls.lower_limit, cls.higher_limit + 1):
+            return True
+        print(f'{cls.__name__} is out of range!')
+        return False
+
+
+class ChargeRate(RangeService):
+    higher_limit = 0.8
+
+    @classmethod
+    def is_value_in_range(cls, value):
+        if value > 0.8:
+            print(f'{cls.__name__} is out of range!')
+            return False
+        return True
+
+
+class StateOfCharge(RangeService):
+    lower_limit = 20
+    higher_limit = 80
+
+    @classmethod
+    def is_value_in_range(cls, value):
+        if value in range(cls.lower_limit, cls.higher_limit + 1):
+            return True
+        print(f'{cls.__name__} is out of range!')
+        return False
+
+
+def battery_is_ok(temperature_value, state_of_charge_value, charge_rate_value):
+    return Temperature.is_value_in_range(temperature_value) and \
+           ChargeRate.is_value_in_range(charge_rate_value) and StateOfCharge.is_value_in_range(state_of_charge_value)
+
+
+def test_battery_is_ok():
+    assert (battery_is_ok(temperature_value=70, state_of_charge_value=20, charge_rate_value=0.8) is False)
+    assert (battery_is_ok(temperature_value=45, state_of_charge_value=20, charge_rate_value=0.8) is True)
+    assert (battery_is_ok(temperature_value=40, state_of_charge_value=10, charge_rate_value=0.8) is False)
+    assert (battery_is_ok(temperature_value=40, state_of_charge_value=50, charge_rate_value=0.8) is True)
+    assert (battery_is_ok(temperature_value=40, state_of_charge_value=30, charge_rate_value=1.0) is False)
+    assert (battery_is_ok(temperature_value=40, state_of_charge_value=50, charge_rate_value=0.6) is True)
 
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    test_battery_is_ok()
